1bb81ecd4d7528709ab7d5f33a93ab65ca999920
MeshalNayim <92199237+MeshalNayim@users.noreply.github.com>
2025-09-24 15:15:22 -0700

    Clean repo, add gitignore, setup for GitHub Pages deploy

--- vite.congif.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// ⚠️ Replace "your-username" with your GitHub username & repo name
-export default defineConfig({
-  plugins: [react()],
-  base: "/meshalnayim.github.io/"
-})
