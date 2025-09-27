e3a8216327174752bca81e99123ff110c42137de
MeshalNayim <92199237+MeshalNayim@users.noreply.github.com>
2025-09-27 02:25:59 -0700

    fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,10 @@
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}"
+    "./src/**/*.{js,jsx,ts,tsx}" // All React components
   ],
-  theme: { extend: {} },
+  theme: {
+    extend: {},
+  },
   plugins: [],
 }
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // <-- Use your repo name here
-})
-
+  base: '/meshalnayim.github.io/', // <-- Use your GitHub repo name here
+});
