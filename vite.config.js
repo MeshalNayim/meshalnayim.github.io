bc0a89d5fe5989af6b9bebc154df1b93e217399a
MeshalNayim <92199237+MeshalNayim@users.noreply.github.com>
2025-09-26 23:51:26 -0700

    updated vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/meshalnayim.github.io/', // <-- Use your repo name here
 })
+
