fec9b80721389d9dd054da9621ba026f207ebbdf
MeshalNayim <92199237+MeshalNayim@users.noreply.github.com>
2025-09-26 22:54:03 -0700

    Setup GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://meshalnayim.github.io/',
+  plugins: [react()],
+  base: '/', // keep "/" for user pages
 })
