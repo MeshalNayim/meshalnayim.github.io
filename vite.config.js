0fe46e962cfb6b61eb87c9d29e56c492b1ca0a21
MeshalNayim <92199237+MeshalNayim@users.noreply.github.com>
2025-09-27 02:32:39 -0700

    fixed

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/meshalnayim.github.io/', // <-- Use your GitHub repo name here
+  base: '/', // <-- Use your GitHub repo name here
 });
